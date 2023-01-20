cf2921551d2a35f2f7d918612e2bebdd68d68b45
Mohsen9april <Mohsen9april@gmail.com>
2023-01-20 15:52:38 +0330

    add sidebar structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import Messages from "./pages/Messages";
+import FileManager from "./pages/FileManager";
+import Analytics from "./pages/Analytics";
+import Order from "./pages/Order";
+import Saved from "./pages/Saved";
+import Setting from "./pages/Setting";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Mehrain pars</h1>
-    </div>
+    <>
+      <h3>Navbar</h3>
+      <div className="App">
+        <div className="Sidebarr">
+          <Sidebar />
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/file-manager" element={<FileManager />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/settings" element={<Setting />} />
+
+          <Route path="*" element={<> not found</>} />
+        </Routes>
+      </div>
+      <h3>Footer</h3>
+    </>
   );
 }
 
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,34 @@
+const Dashboard = () => {
+  return (
+    <div className="title">
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+      <h1>Dashboard</h1>
+      <br />
+    </div>
+  );
+};
+
+export default Dashboard;
