f136bd55b4e20dbc1fb2b3d8a4aacdde0b4c6307
Mohsen9april <Mohsen9april@gmail.com>
2023-02-10 00:55:38 +0330

    modify home page

--- src/pages/Aboutus/Aboutus.jsx
+++ src/pages/Aboutus/Aboutus.jsx
@@ -1,74 +1,34 @@
 import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import "./Aboutus.css";
-
-const AddressMap = () => {
-  return (
-    <div className="google-map-code">
-      {/* <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1681.4468894881884!2d51.33592102034048!3d35.48197532517943!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sde!4v1675186156620!5m2!1sen!2sde"
-        width="600"
-        height="450"
-        allowfullscreen=""
-        className="aboutus_map"
-        tabindex="0"
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-      ></iframe> */}
-
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.2036198683763!2d77.37576731744383!3d28.683554999999984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf07d26db9ccb%3A0xe68c67bf8a9e1201!2sASHOKA%20PAPER%20MILL!5e0!3m2!1sen!2sin!4v1615388020756!5m2!1sen!2sin"
-        width="100%"
-        height="450"
-        // style="border:0;"
-        allowfullscreen=""
-        loading="lazy"
-        className="aboutus_map"
-        data-rocket-lazyload="fitvidscompatible"
-        data-lazy-src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.2036198683763!2d77.37576731744383!3d28.683554999999984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf07d26db9ccb%3A0xe68c67bf8a9e1201!2sASHOKA%20PAPER%20MILL!5e0!3m2!1sen!2sin!4v1615388020756!5m2!1sen!2sin"
-        data-ll-status="loaded"
-        class="entered lazyloaded"
-      ></iframe>
-    </div>
-  );
-};
 
 const Aboutus = () => {
   return (
     <div className="aboutus_container">
-      <div className="aboutus_header"></div>
-      <br />
-      <br />
-      <br />
-      <div className="video_top_aboutus">
-        <video loop autoPlay>
-          <source src="./images/Wecreate.mp4" type="video/mp4" />
-        </video>
+      <div class="responsive-container-block bigContainer">
+        <div class="responsive-container-block Container">
+          <div class="imgContainer">
+            <div>
+              <LazyLoadImage className="mainImg" src="./images/banner3.png" alt="Image Alt" />
+            </div>
+          </div>
+          <div class="responsive-container-block textSide">
+            <h2>درباره ما</h2>
+            <p class="text-blk subHeading">
+              در تاریخ شهریور ماه سال ۱۳۹۸ تاسیس و تحت شماره ۱۳۵۱ در اداره ثبت شرکت های استان تهران
+              به ثبت رسیده است. این شرکت در راستای برنامه پنجم توسعه کشور با هدف تامین بخشی از نیاز
+              داخلی صنعت کاغذ در ایران و کاهش وابستگی به تولیدات خارجی، با ظرفیت سالیانه 22 هزار تن
+              انواع کاغذ و مقوا در تهران قرار گرفته است که به واسطه حضور کارشناسان با تجربه و در
+              اختیار داشتن ماشین آلات روز دنیا و آزمایشگاه های پیشرفته، توانایی رقابت با کلیه کاغذ
+              های داخلی و خارجی را دارا می باشد و تلاش متخصصان این مجموعه جهت دستیابی به تولید
+              محصولات جدید و با کیفیت بالاتر همچنان ادامه دارد.
+            </p>
+            <a href="/" className="button_aboutus">
+              <button class="explore">صفحه اصلی </button>
+            </a>
+          </div>
+        </div>
       </div>
-      <h1>figure </h1>
-      <img
-        style={{ display: "flex", margin: "auto", width: "70rem" }}
-        src="./images/Aboutusone.png"
-        alt="/"
-      />
-      <h1>where we are </h1>
-      <img
-        style={{ display: "flex", margin: "auto", width: "70rem" }}
-        src="./images/Whereweare.png"
-        alt="/"
-      />
-
-      <br />
-      <br />
-
-      <div className="aboutus_map_container">
-        <AddressMap />
-      </div>
-
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,8 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "./Home.css";
 import Customers from "./Customers";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 // export const homeAbout = [
 //   {
@@ -115,11 +117,78 @@ const Home = () => {
       <div className="home_title">
         <h3>کاغذسازی مهرآیین پارس</h3>
       </div>
-      <div className="video_top">
+      {/* <div className="video_top">
         <video loop autoPlay>
           <source src="./images/video.mp4" type="video/mp4" />
         </video>
+      </div> */}
+
+      <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="./images/homeimg1.png" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>کاغذسازی مهرآیین پارس</h5>
+              {/* <p>Some representative placeholder content for the first slide.</p> */}
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="./images/homeimg2.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>انبار صادرات</h5>
+              {/* <p>Some representative placeholder content for the second slide.</p> */}
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="./images/homeimg3.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>خط تولید</h5>
+              {/* <p>Some representative placeholder content for the third slide.</p> */}
+            </div>
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
+
       <br />
       <div
         data-aos-duration="1000"
